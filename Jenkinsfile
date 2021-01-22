284f31aae60c762c55292cceaffc01a09ae8d7d5
Ian Sanders <sandersi@me.com>
2021-01-22 11:58:43 -0600

    starting down a different path - manually getting a token via approle Fri Jan 22 11:58:43 AM CST 2021

--- Jenkinsfile
+++ Jenkinsfile
@@ -7,17 +7,12 @@ pipeline {
                 sh 'true #terraform init'
             }
         }
-        stage('--- Pull Ansible Items ---') {
-            steps {
-                git changelog: false, poll: false, url: 'git@github.com:sandersian/vs_ansible.git'
-            }
-        }
         stage('Terraform - Apply---') {
             steps {
                 withCredentials([usernamePassword(credentialsId: 'vault_terraform_login', passwordVariable: 'SECRET_ID', usernameVariable: 'ROLE_ID')]) {
                     sh '''
                         . ./vault_consul_setup.bash
-                        env > /var/jenkins_home/env.txt
+                        git clone git@github.com:sandersian/vs_ansible.git
                         terraform apply -auto-approve
                     '''
                 }
--- modules/k8s/main.tf
+++ modules/k8s/main.tf
@@ -1,75 +1,79 @@
 terraform {
   required_providers {
     xenorchestra = {
-      source = "terra-farm/xenorchestra"
+      source  = "terra-farm/xenorchestra"
       version = "0.12.0"
     }
     vault = {
-      source = "hashicorp/vault"
+      source  = "hashicorp/vault"
       version = "2.17.0"
     }
   }
 }
 
 provider "xenorchestra" {
-  url       = data.vault_generic_secret.xoa_provisioner.data["url"]
-  username  = data.vault_generic_secret.xoa_provisioner.data["username"]
-  password  = data.vault_generic_secret.xoa_provisioner.data["password"]
+  url      = data.vault_generic_secret.xoa_provisioner.data["url"]
+  username = data.vault_generic_secret.xoa_provisioner.data["username"]
+  password = data.vault_generic_secret.xoa_provisioner.data["password"]
 }
 
 resource "xenorchestra_vm" "worker_nodes" {
-    #count               = length(var.k8s-master-instances)
-    for_each = toset(var.k8s-worker-instances)
-    memory_max          = var.k8s-worker-memory_size
-    cpus                = var.k8s-worker-cpus
-    name_label          = each.key
-    name_description    = each.key
-    template            = var.template-uuid
+  #count               = length(var.k8s-master-instances)
+  for_each         = toset(var.k8s-worker-instances)
+  memory_max       = var.k8s-worker-memory_size
+  cpus             = var.k8s-worker-cpus
+  name_label       = each.key
+  name_description = each.key
+  template         = var.template-uuid
 
-    network {
-      network_id = var.network-uuid
-    }
+  network {
+    network_id = var.network-uuid
+  }
 
-    disk {
-      sr_id             = var.sr-uuid
-      name_label        = "${each.key}-root"
-      size              = var.k8s-worker-disk_size
-    }
+  disk {
+    sr_id      = var.sr-uuid
+    name_label = "${each.key}-root"
+    size       = var.k8s-worker-disk_size
+  }
 
-    provisioner "local-exec" {
-      command           = "cd ~/projects/vs_ansible && ansible-playbook -i ${each.key}, playbook.yml"
-    }
+  provisioner "local-exec" {
+    command = "cd vs_ansible && ansible-playbook -i ${each.key}, k8s-common.yml"
+  }
 
-    provisioner "local-exec" {
-      when              = destroy
-      command           = "cd ~/projects/vs_ansible && ansible -i consul_inventory ${each.key} -u ansible --become -a \"systemctl stop consul\""
-    }
+  provisioner "local-exec" {
+    when    = destroy
+    command = "cd vs_ansible && ansible -i consul_inventory ${each.key} -u ansible --become -a \"systemctl stop consul\""
+  }
 }
 
 resource "xenorchestra_vm" "master_nodes" {
-    for_each = toset(var.k8s-master-instances)
-    memory_max          = var.k8s-master-memory_size
-    cpus                = var.k8s-master-cpus
-    name_label          = each.key
-    name_description    = each.key
-    template            = var.template-uuid
+  for_each         = toset(var.k8s-master-instances)
+  memory_max       = var.k8s-master-memory_size
+  cpus             = var.k8s-master-cpus
+  name_label       = each.key
+  name_description = each.key
+  template         = var.template-uuid
 
-    network {
-      network_id = var.network-uuid
-    }
+  network {
+    network_id = var.network-uuid
+  }
 
-    disk {
-      sr_id             = var.sr-uuid
-      name_label        = "${each.key}-root"
-      size              = var.k8s-master-disk_size
-    }
+  disk {
+    sr_id      = var.sr-uuid
+    name_label = "${each.key}-root"
+    size       = var.k8s-master-disk_size
+  }
 
-    provisioner "local-exec" {
-      command           = "cd ~/projects/vs_ansible && ansible-playbook -i ${each.key}, playbook.yml"
-    }
+  provisioner "local-exec" {
+    command = "cd vs_ansible && ansible-playbook -i ${each.key}, k8s-common.yml"
+  }
 
-    provisioner "local-exec" {
-      when              = destroy
-      command           = "cd ~/projects/vs_ansible && ansible -i consul_inventory ${each.key} -u ansible --become -a \"systemctl stop consul\""
-    }
+  provisioner "local-exec" {
+    command = "cd vs_ansible && ansible-playbook -i ${each.key}, k8s-master.yml"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "cd vs_ansible && ansible -i consul_inventory ${each.key} -u ansible --become -a \"systemctl stop consul\""
+  }
 }
